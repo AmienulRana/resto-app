90cd22a72b998623ca470d760dabbe7e81fe82cc
AmienulRana <amienulrana@gmail.com>
2022-11-29 12:05:42 +0700

    membuat component layout user

---
+++ components/LayoutUser.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+const LayoutUser = (props: Props) => {
+  return (
+    <section className="w-full h-screen flex justify-center">
+      <section className="md:w-96 h-full overflow-y-auto bg-gray-c-100">
+        {props.children}
+      </section>
+    </section>
+  );
+};
+
+export default LayoutUser;
