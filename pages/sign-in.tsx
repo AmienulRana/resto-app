3ef29f0f3c3ff7633ca4e507a2ede97f69eff02d
AmienulRana <amienulrana@gmail.com>
2022-11-29 21:17:44 +0700

    membuat halaman sign-in

---
+++ pages/sign-in.tsx
@@ -0,0 +1,42 @@
+import { Typography } from "@mui/material";
+import LayoutUser from "../components/LayoutUser";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import Link from "next/link";
+
+const SignIn = () => {
+  return (
+    <LayoutUser>
+      <section className="px-6 py-5 mb-6 bg-white">
+        <Typography variant="h6" component="h1">
+          Sign in
+        </Typography>
+        <Typography variant="body1" component="p" className="text-gray-c-200">
+          Find your best ever meal
+        </Typography>
+      </section>
+      <section className="px-6 py-5 bg-white h-4/5">
+        <Input
+          type="text"
+          placeholder="Type your email address"
+          label="Email Address"
+          classNameContainer="mb-4"
+        />
+        <Input
+          type="password"
+          placeholder="Type your password"
+          label="Password"
+          classNameContainer="mb-4"
+        />
+        <Button text="Sign in" classNameProps="bg-orange-c-100" />
+        <Link href="/sign-up">
+          <Button
+            text="Create new account"
+            classNameProps="text-white bg-gray-c-300 mt-3"
+          />
+        </Link>
+      </section>
+    </LayoutUser>
+  );
+};
+export default SignIn;
