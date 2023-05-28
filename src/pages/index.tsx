b837538d49532118b9dd9c811f8f0615aa7f776d
AmienulRana <amienulrana@gmail.com>
2023-05-28 14:06:42 +0700

    create page home food app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,117 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Link from "next/link";
+import { Layout } from "@/components/common/layout";
+import { Header } from "@/components/common/layout/layout"
+import Image from "next/image";
+import { Typography } from "@mui/material";
+import { StarRate, StarOutline } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { Rating } from "@/components/common/rating";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const categorys = ["New Taste", "Populer", "Recommended", "Food", "Drinking"];
 
 export default function Home() {
+  const [indicator, setIndicator] = useState({
+    width: 73,
+    position: 24,
+  });
+  const [tabActive, setTabActive] = useState("");
+
+  const handleNavigateTabs = (button: any, tabActive: string) => {
+    const buttonWidth: number = button.target.clientWidth;
+    const buttonPosition: number = button.target.offsetLeft;
+    setIndicator({ width: buttonWidth, position: buttonPosition });
+    setTabActive(tabActive);
+  };
+  useEffect(() => {
+    setTabActive(categorys[0]);
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <Layout>
+      <Header
+        title="FoodMarket"
+        text="Let’s get some foods"
+        buttonBack={false}
+      />
+      <section className="px-6 flex w-full overflow-x-scroll hide-scrollbar">
+        <section className="min-w-48 h-52 rounded-md overflow-hidden mr-6 bg-white shadow-lg">
+          <div className="w-full h-auto">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/product1.png"
+              width={200}
+              height={140}
+              alt="Product"
+              className="object-cover"
             />
-          </a>
-        </div>
-      </div>
+          </div>
+          <section className="px-3 mt-3">
+            <Typography component="h2" className="text-base truncate">
+              Cherry Healthy
+            </Typography>
+            <div className="flex items-center">
+              {[...Array(4)].map((i) => (
+                <StarRate className="w-4 h-4 text-orange-c-100" key={i} />
+              ))}
+              {[...Array(5 - 4)].map((i) => (
+                <StarOutline className="w-4 h-4 text-gray-c-300" key={i} />
+              ))}
+              <Typography
+                component="p"
+                className="text-gray-c-200 text-xs ml-1.5 mb-0"
+              >
+                4.5
+              </Typography>
+            </div>
+          </section>
+        </section>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <section className="bg-white mt-6">
+        <section className="flex border-b w-full overflow-x-auto hide-scrollbar sticky top-0 border-gray-c-100 px-6 py-4">
+          <span
+            className="absolute bottom-0 rounded border border-black duration-700"
+            style={{
+              left: `${indicator.position}px`,
+              width: `${indicator.width}px`,
+            }}
+          />
+          {categorys.map((data, index) => (
+            <button
+              key={index}
+              className={[
+                "text-md mr-6 bg-transparent min-w-max duration-700",
+                tabActive === data ? " font-bold" : "",
+              ].join(" ")}
+              onClick={(e) => handleNavigateTabs(e, data)}
+            >
+              {data}
+            </button>
+          ))}
+        </section>
+        <section className="px-6 mt-4 pb-2.5">
+          <Link href="/products/123">
+            <section className="flex justify-between mb-3.5">
+              <section className="flex">
+                <Image
+                  src="/product1.png"
+                  width={60}
+                  height={60}
+                  alt="productc"
+                  className="rounded-md mr-3"
+                />
+                <div>
+                  <Typography component="h2">Soup Bumil</Typography>
+                  <Typography component="p" className="text-gray-c-200 text-sm">
+                    Rp289.000
+                  </Typography>
+                </div>
+              </section>
+              < Rating  rating={3.5} />
+            </section>
+          </Link>
+        </section>
+      </section>
+    </Layout>
+  );
 }
